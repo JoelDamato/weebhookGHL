84665f677229e17f0b6410ba8eb6a26d436ff6c8
Tomas Maruccio <tomasmaruccio@gmail.com>
2025-05-29 04:19:38 -0300

    Prueba busqueda en mongo 2

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,7 +1,11 @@
+const Contacto = require('../models/contacto');
 exports.handleWebhook = async (req, res) => {
   console.log('🟡 [DEBUG] Entró al handleWebhook de /webhook');
   try {
-    res.status(200).send({ ok: true, msg: "Respondió rápido para test" }); // <<--- responde rápido
+    console.log('🟡 [DEBUG] Antes de buscar en Mongo');
+    const contacto = await Contacto.findOne({ contact_id: req.body.contact_id });
+    console.log('🟡 [DEBUG] Resultado búsqueda en Mongo:', contacto);
+    res.status(200).send({ ok: true, mongo: contacto });
   } catch (error) {
     console.error('❌ Error en el try del webhook:', error);
     res.status(500).send({ error: 'Error interno del servidor [debug]' });
